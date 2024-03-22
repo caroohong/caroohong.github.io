24b1c1ddb53d6186841c732175f195ab1f1c54e7
Carolina <cbhong@miuandes.cl>
2024-03-22 19:11:26 -0300

    feat:toast/notif when change theme button is toggled

--- script.js
+++ script.js
@@ -5,8 +5,17 @@ function toggleMode() {
     var h2s = document.querySelectorAll("h2");
     var accordion_button = document.querySelectorAll(".accordion-button");
     var accordion_body = document.querySelectorAll(".accordion-body");
+    var accordion_header = document.querySelectorAll(".accordion-header");
+    var toast_header= document.querySelectorAll(".toast-header");
+    var toast_body= document.querySelectorAll(".toast-body");
     var input = document.getElementById("toggle-input-text");
+
+    const toastTrigger = document.getElementById('liveToastBtn')
+    const toastLiveExample = document.getElementById('liveToast')
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+
     if (toggle === "dark") {
+        toastBootstrap.show(); //toast
         body.classList.remove("light-mode");
         body.classList.add("dark-mode");
         input.classList.remove("light-mode");
@@ -18,6 +27,11 @@ function toggleMode() {
             abody.classList.remove("light-mode");
             abody.classList.add("dark-mode");
         })
+        accordion_header.forEach(function(aheader)
+        {
+            aheader.classList.remove("light-mode");
+            aheader.classList.add("dark-mode");
+        })
 
         h1.classList.remove("animated-bright");
         h1.classList.add("animated-dark");
@@ -27,11 +41,22 @@ function toggleMode() {
             abutton.classList.remove("animated-bright");
             abutton.classList.add("animated-dark");
         })
+        toast_body.forEach(function(tbody)
+        {
+            tbody.classList.remove("light-mode");
+            tbody.classList.add("dark-mode");
+        })
+        toast_header.forEach(function(theader)
+        {
+            theader.classList.remove("light-mode");
+            theader.classList.add("dark-mode");
+        })
         h2s.forEach(function(h2) {
             h2.classList.remove("animated-bright");
             h2.classList.add("animated-dark");
         });
     } else if (toggle === "light") {
+        toastBootstrap.show(); //toast
         body.classList.remove("dark-mode");
         body.classList.add("light-mode");
         input.classList.remove("dark-mode");
@@ -45,9 +70,22 @@ function toggleMode() {
             abody.classList.remove("dark-mode");
             abody.classList.add("light-mode");
         })
+        accordion_header.forEach(function(aheader)
+        {
+            aheader.classList.remove("dark-mode");
+            aheader.classList.add("light-mode");
+        })
         accordion_button.forEach(function(abutton){
             abutton.classList.remove("animated-dark");
             abutton.classList.add("animated-bright");
+        })
+        toast_body.forEach(function(tbody){
+            tbody.classList.remove("dark-mode");
+            tbody.classList.add("light-mode");
+        })
+        toast_header.forEach(function(theader){
+            theader.classList.remove("dark-mode");
+            theader.classList.add("light-mode");
         })
         h2s.forEach(function(h2) {
             h2.classList.remove("animated-dark");
@@ -61,3 +99,4 @@ function toggleMode() {
 function abrirEnlace(enlace) {
     window.open(enlace, '_blank');
 }
+
